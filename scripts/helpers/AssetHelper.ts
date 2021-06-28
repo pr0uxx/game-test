1f4a5f037f5464c1ec6a8ffdfc88a290da10b376
Michael Howard <michow289@hotmail.com>
2021-06-28 19:14:02 +0100

    - implemented more generic doTile() in AssetHelper - allowed tiles to be added to groups and layers instead of just layers - added name to new FloorTile based on x/y coords

--- scripts/helpers/AssetHelper.ts
+++ scripts/helpers/AssetHelper.ts
@@ -20,7 +20,7 @@ export class AssetHelper {
 }
 
 export class FloorTile extends Konva.Rect {
-    constructor(x: number, y: number, layer: Konva.Layer, imagePath: string, gif: boolean = false) {
+    constructor(x: number, y: number, layer: Konva.Layer | Konva.Group, imagePath: string, gif: boolean = false) {
         const i = new Image();
         if (gif) {
            Logger.logWarning("gif is not implemented");
@@ -28,7 +28,6 @@ export class FloorTile extends Konva.Rect {
         i.src = imagePath;
         var p = new Promise(res => {
             i.onload = () => {
-                Logger.logInfo('image loaded');
                 layer.add(this);
                 res(null);
             }
@@ -41,11 +40,12 @@ export class FloorTile extends Konva.Rect {
             height: GameGrid.gridSquarePx,
             fillPatternImage: i,
             fillPatternOffsetX: 0,
-            fillPatternOffsetY: 0
+            fillPatternOffsetY: 0,
+            name: `${x}${y}`
         });
     }
 
-    static tileTypes: ((x: number, y: number, layer: Layer) => {})[] = [
+    static tileTypes: ((x: number, y: number, layer: Layer | Konva.Group) => {})[] = [
         FloorTile.dirt,
         FloorTile.flowers,
         FloorTile.water,
@@ -62,84 +62,61 @@ export class FloorTile extends Konva.Rect {
         // FloorTile.soil
     ];
 
-    private static dirt(x: number, y: number, layer: Layer): FloorTile{
-        const r = new FloorTile(x, y, layer, AssetHelper.floorMapPath);
+    private static doTile(index, xOffset: number, yOffest: number, x: number, y: number, layer: Layer | Konva.Group) : FloorTile {
+        let r: FloorTile;
+        r = new FloorTile(x, y, layer, AssetHelper.floorMapPath);
+        r.fillPatternOffsetX(xOffset);
+        r.fillPatternOffsetY(yOffest);
         const scale = 0.5
         r.fillPatternScale({ x: scale, y: scale });
         return r;
     }
 
-    private static flowers(x: number, y: number, layer: Layer): FloorTile{
-        const r = new FloorTile(x, y, layer, AssetHelper.floorMapPath);
-        const scale = 0.5
-        r.fillPatternOffsetX(100);
-        r.fillPatternScale({ x: scale, y: scale });
-        return r;
+    private static dirt(x: number, y: number, layer: Layer | Konva.Group): FloorTile{
+        return FloorTile.doTile(0, 0, 0, x, y, layer);
     }
 
-    private static water(x: number, y: number, layer: Layer): FloorTile {
-        const r = new FloorTile(x, y, layer, AssetHelper.floorMapPath);
-        r.fillPatternOffsetX(200);
-        const scale = 0.5
-        r.fillPatternScale({ x: scale, y: scale });
-        return r;
+    private static flowers(x: number, y: number, layer: Layer | Konva.Group): FloorTile{
+        return FloorTile.doTile(1,100, 0, x, y, layer);
     }
 
-    private static daisies(x: number, y: number, layer: Layer): FloorTile {
-        const r = new FloorTile(x, y, layer, AssetHelper.floorMapPath);
-        r.fillPatternOffsetX(300);
-        const scale = 0.5
-        r.fillPatternScale({ x: scale, y: scale });
-        return r;
+    private static water(x: number, y: number, layer: Layer | Konva.Group): FloorTile {
+        return FloorTile.doTile(2,200, 0, x, y, layer);
     }
 
-    private static grass(x: number, y: number, layer: Layer): FloorTile {
-        const r = new FloorTile(x, y, layer, AssetHelper.floorMapPath);
-        r.fillPatternOffsetX(400);
-        const scale = 0.5
-        r.fillPatternScale({ x: scale, y: scale });
-        return r;
+    private static daisies(x: number, y: number, layer: Layer | Konva.Group): FloorTile {
+        return FloorTile.doTile(3,300, 0, x, y, layer);
     }
 
-    private static soil(x: number, y: number, layer: Layer): FloorTile {
-        const r = new FloorTile(x, y, layer, AssetHelper.floorMapPath);
-        r.fillPatternOffsetX(500);
-        const scale = 0.5
-        r.fillPatternScale({ x: scale, y: scale });
-        return r;
+    private static grass(x: number, y: number, layer: Layer | Konva.Group): FloorTile {
+        return FloorTile.doTile(4,400, 0, x, y, layer);
     }
 
-    private static brick(x: number, y: number, layer: Layer): FloorTile {
-        const r = FloorTile.DefaulFloorTile(x, y, layer);
-        r.fillPatternOffsetX(600);
-        return r;
+    private static soil(x: number, y: number, layer: Layer | Konva.Group): FloorTile {
+        return FloorTile.doTile(5,500, 0, x, y, layer);
     }
 
-    private static hexagonTiles(x: number, y: number, layer: Layer): FloorTile {
-        const r = FloorTile.DefaulFloorTile(x, y, layer);
-        r.fillPatternOffsetX(700);
-        return r;
+    private static brick(x: number, y: number, layer: Layer | Konva.Group): FloorTile {
+        return FloorTile.doTile(6,600, 0, x, y, layer);;
     }
 
-    private static wood(x: number, y: number, layer: Layer): FloorTile {
-        const r = FloorTile.DefaulFloorTile(x, y, layer);
-        r.fillPatternOffsetX(800);
-        return r;
+    private static hexagonTiles(x: number, y: number, layer: Layer | Konva.Group): FloorTile {
+        return FloorTile.doTile(7,700, 0, x, y, layer);
     }
 
-    private static road(x: number, y: number, layer: Layer): FloorTile {
-        const r = FloorTile.DefaulFloorTile(x, y, layer);
-        r.fillPatternOffsetX(900);
-        return r;
+    private static wood(x: number, y: number, layer: Layer | Konva.Group): FloorTile {
+        return FloorTile.doTile(8,800, 0, x, y, layer);
     }
 
-    private static dirtLight(x: number, y: number, layer: Layer): FloorTile {
-        const r = FloorTile.DefaulFloorTile(x, y, layer);
-        r.fillPatternOffsetY(100);
-        return r;
+    private static road(x: number, y: number, layer: Layer | Konva.Group): FloorTile {
+        return FloorTile.doTile(9,900, 0, x, y, layer);
     }
 
-    private static DefaulFloorTile(x: number, y: number, layer: Layer) : FloorTile {
+    private static dirtLight(x: number, y: number, layer: Layer | Konva.Group): FloorTile {
+        return FloorTile.doTile(10,0, 100, x, y, layer);
+    }
+
+    private static DefaulFloorTile(x: number, y: number, layer: Layer | Konva.Group) : FloorTile {
         const r = new FloorTile(x, y, layer, AssetHelper.floorMapPath);
         const scale = 0.5
         r.fillPatternScale({ x: scale, y: scale });
